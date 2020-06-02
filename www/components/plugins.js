05fb619599419c1b328033c05f1284a6a42d62dc
tobirama21 <gabyc5272@gmail.com>
2020-06-03 08:08:47 +0900

    test2

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -2,7 +2,9 @@
 $(document).on("click","#camera",function(){
   navigator.camera.getPicture(onSuccess, onFail, { 
   quality: 50,
-  destinationType: Camera.DestinationType.FILE_URI 
+  destinationType: Camera.DestinationType.FILE_URI,
+  correctOrientation: true,
+  saveToPhotoAlbum: true
   });
 
 function onSuccess(imageURI) {
@@ -13,5 +15,33 @@ function onSuccess(imageURI) {
 function onFail(message) {
     alert('Failed because: ' + message);
 }
+
+});
+
+$(document).on("click","#codigo",function(){
+    cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Resultado: " + result.text + "\n" +
+                "Formato: " + result.format + "\n" +
+                "Cancelamento: " + result.cancelled);
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          preferFrontCamera : false, // iOS and Android
+          showFlipCameraButton : true, // iOS and Android
+          showTorchButton : true, // iOS and Android
+          torchOn: true, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt : "Place a barcode inside the scan area", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+          orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: false // iOS and Android
+      }
+   );
 
 });
